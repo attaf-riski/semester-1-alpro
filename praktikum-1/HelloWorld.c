671cd9de99c88d0e804d3261b6fb9047ded1c63c
attaf-riski <atafriski27@gmail.com>
2022-02-22 11:55:11 +0700

    Selesai Tugas 1

---
+++ praktikum-1/HelloWorld.c
@@ -0,0 +1,13 @@
+// Nama File : HelloWorld.c
+// Deskripsi : Tugas Praktikum Pertama Alpro 2022
+// Pembuat : Attaf Riski Putra Ramadhan -- 24060121120005
+// Tanggal : 22 Februari 2022
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Hello Undip!");
+	return 0;
+}
